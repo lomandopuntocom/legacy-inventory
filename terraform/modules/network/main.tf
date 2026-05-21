793d5f4aa0dfa4255b020242158e957c1c28667e
JeanPaulCabrera <cabrerafloresjeanpaul@gmail.com>
2026-05-20 20:18:17 -0400

    fix: correccion de ip erronea

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -6,13 +6,13 @@ resource "aws_security_group" "app_sg" {
     from_port   = var.app_port
     to_port     = var.app_port
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["190.104.20.154/32"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["190.104.20.154/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
